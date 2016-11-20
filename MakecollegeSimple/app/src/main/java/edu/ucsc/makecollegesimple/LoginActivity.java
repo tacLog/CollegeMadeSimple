fd4099b347341cbaa53d1469037436fd2726b0dc
Grace Ko <grko@ucsc.edu>
2016-11-19 20:43:29 -0800

    changed google sign in code, hopefully working. added sign out option

--- MakecollegeSimple/app/src/main/java/edu/ucsc/makecollegesimple/LoginActivity.java
+++ MakecollegeSimple/app/src/main/java/edu/ucsc/makecollegesimple/LoginActivity.java
@@ -1,150 +1,114 @@
 package edu.ucsc.makecollegesimple;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
-import android.widget.Toast;
-
 import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.auth.api.signin.GoogleSignInResult;
 import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
 
-import org.w3c.dom.Text;
-
-import static edu.ucsc.makecollegesimple.R.id.tvNewUser;
-
-// code written by Belal Kahn from https://www.simplifiedcoding.net/google-login-android-tutorial-integrate-gplus-login/
+// code by Kundan Roy from http://www.androidwarriors.com/2016/02/google-plus-integration-in-android-sign.html
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener, GoogleApiClient.OnConnectionFailedListener {
 
-    //Signin button
-    private SignInButton signInButton;
 
-    //Signing Options
-    private GoogleSignInOptions gso;
-
-    //google api client
-    private GoogleApiClient mGoogleApiClient;
-
-    //Signin constant to check the activity result
-    private int RC_SIGN_IN = 100;
-
-    //TextViews
-    private TextView tvName;
-    private TextView tvEmail;
-
+    TextView tv_username;
+    GoogleApiClient mGoogleApiClient;
+    private static final int RC_SIGN_IN = 9001;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        //Initializing Views
-        tvName = (TextView) findViewById(R.id.tvName);
-        tvEmail = (TextView) findViewById(R.id.tvEmail);
-        final TextView tvNewUser = (TextView) findViewById(R.id.tvNewUser);
-        final TextView tvMenuLink = (TextView) findViewById(R.id.tvMenuLink); // temporary
+        tv_username= (TextView) findViewById(R.id.tv_username);
 
-        //Initializing google signin option
-        gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+        //Register both button and add click listener
+        findViewById(R.id.sign_in_button).setOnClickListener(this);
+        findViewById(R.id.btn_logout).setOnClickListener(this);
+
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
                 .requestEmail()
                 .build();
 
-        //Initializing signinbutton
-        signInButton = (SignInButton) findViewById(R.id.sign_in_button);
-        signInButton.setSize(SignInButton.SIZE_WIDE);
-        signInButton.setScopes(gso.getScopeArray());
-
-        //Initializing google api client
         mGoogleApiClient = new GoogleApiClient.Builder(this)
-                .enableAutoManage(this /* FragmentActivity */, this /* OnConnectionFailedListener */)
+                .enableAutoManage(this, this)
                 .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
                 .build();
-
-
-        //Setting onclick listener to signing button
-        signInButton.setOnClickListener(this);
-
-        // when user clicks newUser, takes them to the welcome activity
-        tvNewUser.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent newUserIntent = new Intent(LoginActivity.this, WelcomeActivity.class);
-
-                LoginActivity.this.startActivity(newUserIntent);
-            }
-        });
-
-        // when user clicks Menu, takes them to the menu activity
-        tvMenuLink.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent menuIntent = new Intent(LoginActivity.this, MainMenu.class);
-
-                LoginActivity.this.startActivity(menuIntent);
-            }
-        });
     }
 
+    @Override
+    public void onClick(View v) {
 
-    //This function will option signing intent
+        switch (v.getId()){
+            case R.id.sign_in_button:
+
+                signIn();
+
+                break;
+            case R.id.btn_logout:
+
+                signOut();
+
+                break;
+        }
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
     private void signIn() {
-        //Creating an intent
         Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
-
-        //Starting intent for result
         startActivityForResult(signInIntent, RC_SIGN_IN);
+
+        Intent signInMenuIntent = new Intent(LoginActivity.this, MainMenu.class);
+
+        LoginActivity.this.startActivity(signInMenuIntent);
+    }
+
+    private void signOut() {
+        Auth.GoogleSignInApi.signOut(mGoogleApiClient).setResultCallback(
+                new ResultCallback<Status>() {
+                    @Override
+                    public void onResult(Status status) {
+                        tv_username.setText("");
+                    }
+                });
     }
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        //If signin
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
         if (requestCode == RC_SIGN_IN) {
             GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
-            //Calling a new function to handle signin
             handleSignInResult(result);
         }
     }
 
-
-    //After the signing we are calling this function
     private void handleSignInResult(GoogleSignInResult result) {
-        //If the login succeed
+
         if (result.isSuccess()) {
-            //Getting google account
+            // Signed in successfully, show authenticated UI.
             GoogleSignInAccount acct = result.getSignInAccount();
-
-            //Displaying name and email
-            tvName.setText(acct.getDisplayName());
-            tvEmail.setText(acct.getEmail());
-
-            // if login is successful, go to main menu
-            Intent loginIntent = new Intent(LoginActivity.this, MainMenu.class);
-            LoginActivity.this.startActivity(loginIntent);
+            tv_username.setText(getString(R.string.signed_in_fmt, acct.getDisplayName()));
 
         } else {
-            //If login fails
-            Toast.makeText(this, "Login Failed", Toast.LENGTH_LONG).show();
+            // Signed out, show unauthenticated UI.
+            // updateUI(false);
         }
-    }
-
-    @Override
-    public void onClick(View v) {
-        if (v == signInButton) {
-            //Calling signin
-            signIn();
-        }
-    }
-
-    @Override
-    public void onConnectionFailed(ConnectionResult connectionResult) {
-
     }
 }
