4ec4dbce4c77f9151930a4fd907b514a5b1ceaeb
tcahill$ <tcahill@ucsc.edu>
2017-03-15 16:48:18 -0700

    more things work

--- college-made-simple/models/Numbers.js
+++ college-made-simple/models/Numbers.js
@@ -1,19 +1,208 @@
 var mongoose = require('mongoose');
+var GenerateSchema = require('generate-schema');
 
-var NumbersSchema = new mongoose.Schema({
-	title: {type: String, default: "Welcome"},
-	jobs: {type: Number, default: 0},
-	/*
-	scholarships: {type: Number, default: 0},
-	other: {type: Number, default: 0},
-	grants: {type: Number, default: 0},
-	parents: {type: Number, default: 0},
-	job: {type: Number, default: 0},
-	utilities: {type: Number, default: 0},
-	other: {type: Number, default: 0},
-	books: {type: Number, default: 0},
-	food: {type: Number, default: 0},
-	rent: {type: Number, default: 0},
-	*/
-});
+var schema = GenerateSchema.mongoose(
+{
+	"ID":"NONE",
+	"numbers": [
+	{"id" : "error",
+	"total":"50",
+
+	"value1": "10",
+	"title1": "Sample Title",
+	"value2": "10",
+	"title2": "Sample Title",
+	"value3": "10",
+	"title3": "Sample Title",
+	"value4": "10",
+	"title4": "Sample Title",
+	"value5": "10",
+	"title5": "Sample Title"
+
+},
+{"id" : "scholarships",
+"total":"50",
+
+"value1": "10",
+"title1": "Sample Title",
+"value2": "10",
+"title2": "Sample Title",
+"value3": "10",
+"title3": "Sample Title",
+"value4": "10",
+"title4": "Sample Title",
+"value5": "10",
+"title5": "Sample Title"
+
+},
+{"id" : "otherIn",
+"total":"50",
+
+"value1": "10",
+"title1": "Sample Title",
+"value2": "10",
+"title2": "Sample Title",
+"value3": "10",
+"title3": "Sample Title",
+"value4": "10",
+"title4": "Sample Title",
+"value5": "10",
+"title5": "Sample Title"
+
+},
+{"id" : "grants",
+"total":"50",
+
+"value1": "10",
+"title1": "Sample Title",
+"value2": "10",
+"title2": "Sample Title",
+"value3": "10",
+"title3": "Sample Title",
+"value4": "10",
+"title4": "Sample Title",
+"value5": "10",
+"title5": "Sample Title"
+
+},
+{"id" : "parents",
+"total":"50",
+
+"value1": "10",
+"title1": "Sample Title",
+"value2": "10",
+"title2": "Sample Title",
+"value3": "10",
+"title3": "Sample Title",
+"value4": "10",
+"title4": "Sample Title",
+"value5": "10",
+"title5": "Sample Title"
+
+},
+{"id" : "job",
+"total":"50",
+
+"value1": "10",
+"title1": "Sample Title",
+"value2": "10",
+"title2": "Sample Title",
+"value3": "10",
+"title3": "Sample Title",
+"value4": "10",
+"title4": "Sample Title",
+"value5": "10",
+"title5": "Sample Title"
+
+},
+{"id" : "utilities",
+"total":"50",
+
+"value1": "10",
+"title1": "Sample Title",
+"value2": "10",
+"title2": "Sample Title",
+"value3": "10",
+"title3": "Sample Title",
+"value4": "10",
+"title4": "Sample Title",
+"value5": "10",
+"title5": "Sample Title"
+},
+
+{"id" : "personal",
+"total":"150",
+
+"value1": "10",
+"title1": "Sample Title",
+"value2": "10",
+"title2": "Sample Title",
+"value3": "10",
+"title3": "Sample Title",
+"value4": "10",
+"title4": "Sample Title",
+"value5": "10",
+"title5": "Sample Title"
+
+},
+{"id" : "food",
+"total":"50",
+
+"value1": "10",
+"title1": "Sample Title",
+"value2": "10",
+"title2": "Sample Title",
+"value3": "10",
+"title3": "Sample Title",
+"value4": "10",
+"title4": "Sample Title",
+"value5": "10",
+"title5": "Sample Title"
+
+},
+{"id" : "rent",
+"total":"50",
+
+"value1": "10",
+"title1": "Sample Title",
+"value2": "10",
+"title2": "Sample Title",
+"value3": "10",
+"title3": "Sample Title",
+"value4": "10",
+"title4": "Sample Title",
+"value5": "10",
+"title5": "Sample Title"
+
+},
+{"id" : "tuition",
+"total":"50",
+
+"value1": "10",
+"title1": "Sample Title",
+"value2": "10",
+"title2": "Sample Title",
+"value3": "10",
+"title3": "Sample Title",
+"value4": "10",
+"title4": "Sample Title",
+"value5": "10",
+"title5": "Sample Title"
+
+},
+{"id" : "supplies",
+"total":"50",
+
+"value1": "10",
+"title1": "Sample Title",
+"value2": "10",
+"title2": "Sample Title",
+"value3": "10",
+"title3": "Sample Title",
+"value4": "10",
+"title4": "Sample Title",
+"value5": "10",
+"title5": "Sample Title"
+
+},
+{"id" : "transportation",
+"total":"50",
+
+"value1": "10",
+"title1": "Sample Title",
+"value2": "10",
+"title2": "Sample Title",
+"value3": "10",
+"title3": "Sample Title",
+"value4": "10",
+"title4": "Sample Title",
+"value5": "10",
+"title5": "Sample Title"
+
+}
+]
+}
+);
+var NumbersSchema = new mongoose.Schema(
+	schema);
 mongoose.model('Numbers', NumbersSchema);
--- college-made-simple/public/javascripts/angularApp.js
+++ college-made-simple/public/javascripts/angularApp.js
@@ -1,3 +1,18 @@
+var MyHelpers = MyHelpers ||{};
+
+MyHelpers.helpers = {
+	loadById: function(numbers, string){
+		for (var i = numbers.length - 1; i >= 0; i--) {
+			if(numbers[i].id==string){
+				return i;
+			}
+
+		}
+		return 0;
+	}
+}
+
+
 var app = angular.module('materializeApp', ['ui.materialize','ui.router','chart.js'])
 
 app.factory('numbers', ['$http', function($http){
@@ -27,6 +42,13 @@ app.factory('numbers', ['$http', function($http){
 			angular.copy(data, o.current);
 		});
 	};
+
+	o.update = function(section, id){
+		this.default[id]= section;
+		return $http.post('/numbers', this.default).success(function(data){
+			console.log(data);
+		})
+	}
 
 	return o;
 }]);
@@ -73,27 +95,87 @@ app.controller('SumController', ['$scope',
 	'numbers',
 	function ($scope, numbers) {
 		$scope.test = 'Hello world';
+		//load the numbers
 		$scope.numbers = numbers.default;
-		$scope.display = {};
-		$scope.lables = ["scholarships", "grants", "job", "parents", "other", "utilities", "books", "food", "rent", "tuition"];
+		//load my helpers
+		$scope.helpers = MyHelpers.helpers;
+
+		//testing
+		//console.log($scope.helpers.loadById($scope.numbers,"grants"));
+		
+		//labels for the graphs
+		$scope.lables = ["scholarships", "grants", "job", "parents", "other", "utilities", "other", "food", "rent", "tuition"];
+		//graph for the in side
 		$scope.labelsIN = ["Scholarships", "Grants", "Job", "Parents", "Other"];
-		$scope.dataIN = [$scope.numbers.scholarships.total,$scope.numbers.grants.total,$scope.numbers.job.total,$scope.numbers.parents.total,$scope.numbers.other.total];
-		$scope.labelsOUT = ["Utilities", "Books", "Food", "Rent", "Tuition"];
-		$scope.dataOUT = [$scope.numbers.utilities.total,$scope.numbers.books.total,$scope.numbers.food.total,$scope.numbers.rent.total,$scope.numbers.tuition.total];
+		$scope.dataIN = [$scope.numbers[$scope.helpers.loadById($scope.numbers,"scholarships")].total,$scope.numbers[$scope.helpers.loadById($scope.numbers,"grants")].total,$scope.numbers[$scope.helpers.loadById($scope.numbers,"job")].total,$scope.numbers[$scope.helpers.loadById($scope.numbers,"parents")].total,$scope.numbers[$scope.helpers.loadById($scope.numbers,"otherIn")].total];
+		//graph for the out side
+		$scope.labelsOUT = ["Utilities", "Personal", "Food", "Rent", "Tuition", "Supplies", "Transportation"];
+		$scope.dataOUT = [$scope.numbers[$scope.helpers.loadById($scope.numbers,"utilities")].total,$scope.numbers[$scope.helpers.loadById($scope.numbers,"personal")].total,$scope.numbers[$scope.helpers.loadById($scope.numbers,"food")].total,$scope.numbers[$scope.helpers.loadById($scope.numbers,"rent")].total,$scope.numbers[$scope.helpers.loadById($scope.numbers,"tuition")].total,$scope.numbers[$scope.helpers.loadById($scope.numbers,"tuition")].supplies,$scope.numbers[$scope.helpers.loadById($scope.numbers,"tuition")].transportation];
+		
+		//loading the sumary screen
+		$scope.tuition = $scope.numbers[$scope.helpers.loadById($scope.numbers,"tuition")].total;
+		$scope.personal =$scope.numbers[$scope.helpers.loadById($scope.numbers,"personal")].total;
+		$scope.supplies = $scope.numbers[$scope.helpers.loadById($scope.numbers,"supplies")].total;
+		$scope.rent = $scope.numbers[$scope.helpers.loadById($scope.numbers,"rent")].total;
+		$scope.transportation = $scope.numbers[$scope.helpers.loadById($scope.numbers,"transportation")].total; 
+		$scope.personal = $scope.numbers[$scope.helpers.loadById($scope.numbers,"personal")].total;
+		$scope.totalOut = ($scope.tuition-0) + ($scope.personal-0) + ($scope.supplies-0) + ($scope.rent-0)
+		+ ($scope.transportation-0) + ($scope.personal-0);
+
 		console.log($scope.numbers);
+		
 	}]);
 
 app.controller('EditController', [
 	'$scope',
 	'numbers',
-	function ($scope, numbers) {
-		$scope.test = 'Hello world';
-		$scope.edit = function(){
-			if(!$scope.name || $scope.name === ''){return;}
-			numbers.create({
-				title: $scope.name,
-				jobs: $scope.jobs,
-			});
+	'sourceId',
+	function ($scope, numbers, sourceId) {
+		//load my helpers
+		$scope.helpers = MyHelpers.helpers;
+		$scope.numbers = numbers.default;
+		//testing
+		//console.log($scope.numbers);
+		//console.log(sourceId);
+		var id = $scope.helpers.loadById($scope.numbers,sourceId);
+		//console.log(id);
+		if(id==0){
+			console.log('error help!');
+		}
+		else {
+			$scope.value1=$scope.numbers[id].value1;
+			$scope.value2=$scope.numbers[id].value2;
+			$scope.value3=$scope.numbers[id].value3;
+			$scope.value4=$scope.numbers[id].value4;
+			$scope.value5=$scope.numbers[id].value5;
+			$scope.title1= $scope.numbers[id].title1;
+			$scope.title2= $scope.numbers[id].title2;
+			$scope.title3= $scope.numbers[id].title3;
+			$scope.title4= $scope.numbers[id].title4;
+			$scope.title5= $scope.numbers[id].title5;
+		}
+		$scope.update = function(){
+			//console.log("it works");
+			var total = ($scope.value1-0) + ($scope.value2-0) +($scope.value3-0) +($scope.value4-0) +($scope.value5-0);
+			var tot = total.toString();
+			var temp = {
+				"id" : sourceId,
+				"total":tot,
+
+				"value1": $scope.value1,
+				"title1": $scope.title1,
+				"value2": $scope.value2,
+				"title2": $scope.title2,
+				"value3": $scope.value3,
+				"title3": $scope.title3,
+				"value4": $scope.value4,
+				"title4": $scope.title4,
+				"value5": $scope.value5,
+				"title5": $scope.title5
+
+			};
+			numbers.update(temp,sourceId)
+			console.log(temp);
 		}
 	}]);
 
@@ -116,9 +198,17 @@ app.config([
 
 		$stateProvider
 		.state('edit', {
-			url: '/edit',
+			url: '/edit/{editId}',
 			templateUrl: '/edit.html',
-			controller: 'EditController'
+			controller: 'EditController',
+			resolve: {
+				sourceId: ['$stateParams', function($stateParams) {
+					return $stateParams.editId;
+				}],
+				postPromise: ['numbers', function(numbers){
+					return numbers.getAll();
+				}]
+			}
 		});
 
 		$urlRouterProvider.otherwise('home');
